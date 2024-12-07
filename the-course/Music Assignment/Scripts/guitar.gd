93f8dced5a437c4a55d1d73618e3f3349b340742
RonanCanales <canalesronan@yahoo.ie>
2024-12-07 15:47:24 +0000

    Buttons+Volume slider

--- the-course/Music Assignment/Scripts/guitar.gd
+++ the-course/Music Assignment/Scripts/guitar.gd
@@ -7,6 +7,9 @@ var g_string_sound = preload("res://Music Assignment/Strings sounds/acoustic-gui
 var b_string_sound = preload("res://Music Assignment/Strings sounds/acoustic-guitar-string-b-note-low.wav")
 var high_e_sound = preload("res://Music Assignment/Strings sounds/acoustic-guitar-string-e-note-high.wav")
 
+
+@onready var volume_slider: HSlider = $VolumeSlider
+
 #Guitar Chords
 var c_major = preload("res://Music Assignment/Chord  sounds/17569__danglada__c-major-[AudioTrimmer.com].wav")
 var g_major = preload("res://Music Assignment/Chord  sounds/17573__danglada__g-major-[AudioTrimmer.com].wav")
@@ -14,53 +17,54 @@ var f_major = preload("res://Music Assignment/Chord  sounds/17572__danglada__f-m
 var d_major = preload("res://Music Assignment/Chord  sounds/17570__danglada__d-major-[AudioTrimmer.com].wav")
 var a_major = preload("res://Music Assignment/Chord  sounds/17567__danglada__a-major-[AudioTrimmer.com].wav")
 var b_major = preload("res://Music Assignment/Chord  sounds/17568__danglada__b-major-[AudioTrimmer.com].wav")
-func _on_button_low_e_pressed() -> void:
-	play_sound(low_e_sound)
 
+
+func _on_button_low_e_pressed() -> void:
+	play_sound(low_e_sound, volume_slider.value)
 
 func _on_button_a_pressed() -> void:
-	play_sound(a_string_sound)
+	play_sound(a_string_sound, volume_slider.value)
 
 
 func _on_button_d_pressed() -> void:
-	play_sound(d_string_sound)
+	play_sound(d_string_sound, volume_slider.value)
 
 func _on_button_g_pressed() -> void:
-	play_sound(g_string_sound)
+	play_sound(g_string_sound, volume_slider.value)
 
 func _on_button_b_pressed() -> void:
-	play_sound(b_string_sound)
+	play_sound(b_string_sound, volume_slider.value)
 
 func _on_button_high_e_pressed() -> void:
-	play_sound(high_e_sound)
+	play_sound(high_e_sound, volume_slider.value)
 
 
-func play_sound(sound: AudioStream):
+func play_sound(sound: AudioStream, volume: float):
 	var audio_player = AudioStreamPlayer.new()
 	add_child(audio_player)
 	audio_player.stream = sound
+	audio_player.volume_db = linear_to_db(volume)
 	audio_player.play()
 
 
 func _on_button_c_chord_pressed() -> void:
-	play_sound(c_major)
+	play_sound(c_major, volume_slider.value)
 
 
 func _on_button_g_major_pressed() -> void:
-	play_sound(g_major)
+	play_sound(g_major, volume_slider.value)
 
 
 func _on_button_f_major_pressed() -> void:
-	play_sound(f_major)
+	play_sound(f_major, volume_slider.value)
 
 
 func _on_button_d_major_pressed() -> void:
-	play_sound(d_major)
-
+	play_sound(d_major, volume_slider.value)
 
 func _on_button_a_major_pressed() -> void:
-	play_sound(a_major)
+	play_sound(a_major, volume_slider.value)
 
 
 func _on_button_b_major_pressed() -> void:
-	play_sound(b_major)
+	play_sound(b_major, volume_slider.value)
