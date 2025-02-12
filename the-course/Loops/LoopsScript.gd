2c00c768d4c7ab7efa667a335415adad0714d619
RonanCanales <canalesronan@yahoo.ie>
2025-02-12 13:49:39 +0000

    Nested Loops
    Nested Loops

--- the-course/Loops/LoopsScript.gd
+++ the-course/Loops/LoopsScript.gd
@@ -3,14 +3,20 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range (1, 11):
+	for i in range (1, 11): #For
 		print (i)
 
-	var i = 10
+	var i = 10 #While
 	while i > 0:
 		print(i)
 		i -= 1
-	pass # Replace with function body.
+
+	for x in range(3): # Nested Loops
+		var row = ""
+		for y in range(3):
+			row += "(" + str(x) + ", " + str(y) + ") "
+		print (row)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
