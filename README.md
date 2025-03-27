9777414b4fdb90de0337a7b9f6d85e9d8e2899ff
RonanCanales <canalesronan@yahoo.ie>
2025-03-27 21:05:18 +0000

    shoooter
    shooter update

--- README.md
+++ README.md
@@ -52,6 +52,38 @@ Weekly engagement mark is assessessed automatically by evaluating the number of 
 
 # Semester 2
 
+## Week 8 - 3D character animation
+- [Animation Blending Part 1](https://www.youtube.com/watch?v=iElHZhOxGYA)
+- [Animation Blending Part 2](https://www.youtube.com/watch?v=iElHZhOxGYA)
+- [Look at](https://www.youtube.com/watch?v=iElHZhOxGYA)
+- [Animation Trees](https://www.youtube.com/watch?v=iElHZhOxGYA)
+- [Vuntra City](https://www.youtube.com/watch?v=H-cL8zlNsGI)
+- [Godot 3D Characters](https://github.com/gdquest-demos/godot-4-3D-Characters)
+
+## Lab
+- See Brightspace for the lab
+
+## Week 7 - GDC
+
+## Week 6 - 3D FPS
+- [3D nodes](labs/3d.md)
+
+### Lab Exercise
+- [Create a 3D endless runner](labs/endless_runner.md)
+
+## Week 5 - Enemy AI
+- [Air Sea Battle](https://www.retrogames.cz/play_349-Atari2600.php)
+- [Lab](labs/enemy_ai.md)
+
+## Week 4 - Tweens & Juice
+- [Tweens](labs/tweens.md)
+- [Tweens lab](labs/tweens_lab.md)
+
+## Week 3
+- Shooter Game
+
+## [Shooter Game Exercises](labs/shooter_game_lab.md)
+
 ## Week 2 
 - More procedural drawing
 - [Spirals] - Have a look at the spirals scene!
--- the-course/dude_bullet.gd
+++ the-course/dude_bullet.gd
@@ -9,23 +9,47 @@ extends CharacterBody2D
 @export var line_size:float = 3
 
 
+func _ready() -> void:
+	# randomly vary pitch
+	$LaserShoot.pitch_scale = randf_range(0.1, 2)
 
 func _draw():
+	# a single line
 	draw_line(Vector2(0, 10), Vector2(0, -10), color, line_size)
-
 	
 func _process(delta):
+	# move in my forward direction
 	velocity = -transform.y * speed
 	var c =  move_and_collide(velocity * delta)
-	if c and c.get_collider().is_in_group("ufo"):  # Layer 2 (example)
-		print("Collided with a ufo!")	
-		c.get_collider().queue_free()
+	if c: 
+		var coll = c.get_collider()
+		if coll.is_in_group("ufo"):
+			print("collided with a ufo")	
+			# update the ufo count			
+			var uc = $"../dude".ufo_count
+			$"../dude".ufo_count = uc + 1
+			$"../CanvasLayer/ufo_count".text = "UFO COUNT:" + str($"../dude".ufo_count)
+			# delete the ufo
+			c.get_collider().drop_loot()
+			c.get_collider().queue_free()
+			
+		elif coll.is_in_group("dude"):
+			coll.lives = coll.lives - 1
+			print("collided with a dude")
+			# respawn the dude
+			coll.respawn()
+			pass
+		# create an explosion
+		
+		
+		
 		var e = explosion.instantiate()
-		e.modulate = c.get_collider().color
-		get_tree().get_root().add_child(e)
+		e.modulate = coll.color
+		get_parent().add_child(e)
 		e.global_position = self.global_position
 		e.emitting = true
 		self.queue_free()
+		
 
 func _on_timer_timeout():
 	self.queue_free()
